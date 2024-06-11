7a95506163be21aab60d56daef53e3e28597c041
Richard Benim <bricharddarling@gmail.com>
2024-06-11 02:26:37 +0000

    add my first lesson

---
+++ hello.py
@@ -0,0 +1,22 @@
+
+#using formatted string
+name = input("Name: ")
+print(f"Hello, {name}");
+
+#conditions
+n = int(input("Number: "))
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+ print("n is zero");
+
+ #Sequence. selecting a sprcific letter in a
+names = input("Names: ")
+print(names[2])
+
+plist = ["many", "Kofi", "Yaw"]
+print(plist[1])
+
+
