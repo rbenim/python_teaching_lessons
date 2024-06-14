1f4af36ff1aa1e16e34f84cf26646e44d6770f5d
Richard Benim <bricharddarling@gmail.com>
2024-06-14 21:40:55 +0000

    python dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+#Dictionary is like storing one data in a another 
+Countries = {"Africa": "Ghana",  "Asia": "China"}
+print(Countries["Africa"])
+
+#we can also assign another value to the GHana key
+Countries["my_homeland"] = "Ghana"
+print(Countries["my_homeland"])
+
